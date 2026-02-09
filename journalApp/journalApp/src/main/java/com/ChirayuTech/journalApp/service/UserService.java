786c071ddb099df86f8626ba160a10ebd9f0c097
ChirayuP19 <chirayu010675@gmail.com>
2026-02-09 01:12:00 -0500

    Add passworder encoder and save encode password

--- journalApp/journalApp/src/main/java/com/ChirayuTech/journalApp/service/UserService.java
+++ journalApp/journalApp/src/main/java/com/ChirayuTech/journalApp/service/UserService.java
@@ -6,8 +6,11 @@ import com.ChirayuTech.journalApp.repository.JournalEntryRepo;
 import com.ChirayuTech.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,9 +20,18 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    private static final PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+
     public User saveEntry(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
+
+//    public User saveNewUser(User user){
+//        user.setPassword(passwordEncoder.encode(user.getPassword()));
+//        user.setRoles(Arrays.asList("USER"));
+//        return userRepository.save(user);
+//    }
 
     public List<User> getAll(){
        return userRepository.findAll();
